71e0d3d8c6c04ac98e37e6a1942e416e671e8c0b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-10-28 11:30:59 +0530

    Setup Command-Line Argument Parsing Logic
    - Creates the `title` and `message` properties in the `Program` class
    - Creates `ParseArgs` method to parse the command-line-arguments (`args`) into the properties
    - Sets up the VS Code debug configuration for the cli application

--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
 ﻿class Program
 {
+    /// <summary>The notification title</summary>
+    static string title = "";
+    /// <summary>The notification message</summary>
+    static string message = "";
+
+
     static void Main(string[] args)
     {
-        foreach (string arg in args)
+        // Parse the command-line-arguments
+        ParseArgs(args);
+
+        Console.WriteLine($"title: {title}");
+        Console.WriteLine($"message: {message}");
+    }
+
+    /// <summary>
+    /// Parses the given command-line-arguments
+    /// </summary>
+    /// <param name="args">The command-line arguments coming in from Main</param>
+    private static void ParseArgs(string[] args)
+    {
+        List<string> positionalArguments = [];
+        for (int i = 0; i < args.Length; i++)
         {
-            Console.WriteLine(arg);
+            switch (args[i])
+            {
+                // Check for flags and extract their values
+                case "-t":
+                case "--title":
+                    if (i + 1 < args.Length) title = args[++i];
+                    break;
+                case "-m":
+                case "--message":
+                case "--body":
+                case "--contents":
+                    if (i + 1 < args.Length) message = args[++i];
+                    break;
+                default:
+                    // Everything that isn't a flag is stored as a positional argument
+                    positionalArguments.Add(args[i]);
+                    break;
+            }
+        }
+        // if the `title` is still empty, check if we can take the first positional argument
+        if (string.IsNullOrEmpty(title) && positionalArguments.Count > 0)
+        {
+            title = positionalArguments[0];
+        }
+        // If the `message` is still empty, check if we can take the second positional argument
+        if (string.IsNullOrEmpty(message) && positionalArguments.Count > 1)
+        {
+            message = positionalArguments[1];
         }
     }
 }
